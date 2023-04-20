6daff1e2291a27de383823c5f1933d8abeb23a44
yoon <hayashitaro1902@gmail.com>
2023-04-20 02:18:59 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MicropostsController;
+use App\Http\Controllers\UserFollowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,12 @@ Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['a
 require __DIR__.'/auth.php';
 
 Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow');
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings');
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');
+    });
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });
